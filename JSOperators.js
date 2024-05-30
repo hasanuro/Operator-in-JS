24143f303b884f313cfc6ad9fc38ac53634673c0
hasanuro <148393835+hasanuro@users.noreply.github.com>
2024-05-31 00:47:08 +0500

    Add files via upload

---
+++ JSOperators.js
@@ -0,0 +1,93 @@
+// let num1 = 50;
+// let num2 = 15;
+// num1 = num1 + num2;
+// document.write(num1);
+
+// -------------------------------------Assignment Operator----------------------------------------
+
+// 1) +=
+// num1 += num2;
+// document.write(num1);
+
+// 2) -=
+// num1 -= num2;
+// document.write(num1);
+
+// 3) *=
+// num1 *= num2;
+// document.write(num1);
+
+// 4) /=
+// num1 /= num2;
+// document.write(num1);
+
+// 5) **=
+// num1 **= num2;
+// document.write(num1);
+
+// 6) %=
+// num1 %= num2;
+// document.write(num1);
+
+
+// -----------------------------------------Comparison Operator-------------------------------------
+// > < >= <= == === != !==
+
+// 1) >
+// var x = 20;
+// var y = 10;
+// document.write("x > y : " , x>y);
+
+//2) <
+// var x = 20;
+// var y = 10;
+// document.write("x < y : " , x<y);
+
+//3) <=
+// var x = 20;
+// var y = 10;
+// document.write("x <= y : " , x<=y);
+
+
+//4) >=
+// var x = 20;
+// var y = 10;
+// document.write("x >= y : " , x>=y);
+
+//5) ==
+// var x = "10";
+// var y = 10;
+// document.write("x == y : " , x==y);
+
+//6) ===
+// var x = "10";
+// var y = 10;
+// document.write("x === y : " , x===y);
+
+//7) !=
+// var x = "20";
+// var y = 10;
+// document.write("x != y : " , x!=y);
+
+//8) !==
+// var x = "10";
+// var y = 10;
+// document.write("x !== y : " , x!==y);
+
+// -----------------------------------------Logical Operator-------------------------------------
+// && || !
+
+// 1) &&
+// var val1 = 45;
+// var val2 =5;
+// document.write(val1 > val2 && val2 == 5);
+
+// 2) ||
+// var val1 = 45;
+// var val2 =5;
+// document.write(val1 < val2 || val2 == 5);
+
+// 3) !
+var val1 = 45;
+var val2 =5;
+document.write(!(val1 < val2 || val2 == 5));
